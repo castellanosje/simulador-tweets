ed48933e9a12745ba62b165bb541ecacf161c551
je34429 <je34429@gmail.com>
2021-03-09 15:48:59 -0500

    agregando docs para github pages

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Menu :openCloseForm = "openCloseForm" :showForm="showForm"  />
+  <TweetForm :showForm="showForm" :openCloseForm = "openCloseForm" :reloadTweets = "reloadTweets" />
+   <TweetList :tweets="tweets" :reloadTweets = "reloadTweets" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue';
+import Menu from './components/Menu';
+import TweetForm from './components/TweetForm';
+import TweetList from './components/TweetList'
+import useFormTweet from './hooks/useFormTweets';
+import { getTweetsApi } from './api/tweet'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu,
+    TweetForm,
+    TweetList
+  },
+  setup(){
+    let tweets = ref(getTweetsApi().reverse());
+    const {openCloseForm, showForm} = useFormTweet();
+
+    const reloadTweets = () =>{
+      
+      console.log("se ejecuta");
+      tweets.value = getTweetsApi().reverse();
+    }
+    
+    
+    return{
+        showForm,
+        openCloseForm,
+        tweets,
+        reloadTweets
+    }
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
